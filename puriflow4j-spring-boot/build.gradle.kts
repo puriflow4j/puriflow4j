088b684e6285f9c237294f10d50d525e64b3f061
Violetta Podvolotskaya <violettapidvolotska@gmail.com>
2025-10-20 19:50:18 +0200

    Added new logs modules

--- puriflow4j-spring-boot/build.gradle.kts
+++ puriflow4j-spring-boot/build.gradle.kts
@@ -2,7 +2,7 @@ plugins { `java-library` }
 
 dependencies {
     api(project(":puriflow4j-core"))
-    api(project(":puriflow4j-logs"))
+    api(project(":puriflow4j-logs-logback"))
     compileOnly("org.springframework.boot:spring-boot-autoconfigure:3.3.4")
     compileOnly("org.springframework.boot:spring-boot-starter-actuator:3.3.4")
     compileOnly("io.micrometer:micrometer-core:1.13.4")
